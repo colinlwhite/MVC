20127281a14bcefb2eca3e32bada882fcefc3863
Colin White <clwhite814@gmail.com>
2021-04-22 18:09:58 -0500

    updated Startup.cs with Razor Pages comments

--- Startup.cs
+++ Startup.cs
@@ -23,6 +23,7 @@ namespace MVC
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Adding Razor Pages to the MVC application
             services.AddRazorPages();
             services.AddControllersWithViews();
         }
